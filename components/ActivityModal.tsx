fd06080b099e86baeb034aa9eae93a5e2fe99322
Santiago Espinosa <103274976+Santyet@users.noreply.github.com>
2024-11-25 23:18:05 -0500

    Enhance ActivityModal with clickable elements and state management for user interaction

--- components/ActivityModal.tsx
+++ components/ActivityModal.tsx
@@ -1,40 +1,113 @@
 import React, { useState } from 'react';
-import Question from '@/interfaces/Question';
-// @ts-ignore
-import Article from '@/interfaces/Article';
-import ArticleModal from '@/components/ArticleModal';
-import QuestionsModal from '@/components/QuestionsModal';
+import ArticleModal from './ArticleModal'; // Import the ArticleModal component
+import QuestionsModal from './QuestionsModal';
 
 interface ActivityModalProps {
-  onClose: (score: number, isCompleted: boolean) => void;
-  questions: Question[];
-  articles: Article[];
+  onClose: () => void;
+  questions: any[];
+  articles: any[];
 }
 
-const ActivityModal: React.FC<ActivityModalProps> = ({
-  onClose,
-  questions,
-  articles,
-}) => {
+const ActivityModal: React.FC<ActivityModalProps> = ({ onClose, questions }) => {
+
+
   const [isArticlesRead, setIsArticlesRead] = useState<boolean>(false);
 
   const handleArticleFinish = () => {
     setIsArticlesRead(true);
   };
 
+  const [clickedButtons, setClickedButtons] = useState({
+    element1: false,
+    element2: false,
+    element3: false,
+    element4: false,
+    element5: false,
+    element6: false,
+  });
+
+  const handleClick = (element: string) => {
+    setClickedButtons(prevState => ({
+      ...prevState,
+      [element]: true,
+    }));
+  };
+
+  const allButtonsClicked = Object.values(clickedButtons).every(Boolean);
+
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center ">
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="z-50 p-4 bg-white border rounded-lg shadow-lg w-5/6 h-auto flex">
-        {!isArticlesRead ? (
-          <ArticleModal
-            onClose={onClose}
-            articles={articles}
-            initQuestions={handleArticleFinish}
-          />
-        ) : (
-          <QuestionsModal onClose={onClose} questions={questions} />
-        )}
+        <div className="relative w-full h-full">
+            {!allButtonsClicked ? (
+            <div className="relative w-2/3 h-2/3 mx-auto">
+              <img src="/img/clickTest.png" alt="Background" className="w-full h-full object-cover" />
+              <div
+              className="absolute"
+              style={{ top: '90px', left: '96px' }}
+              onClick={() => handleClick('element1')}
+              >
+              <button className="bg-transparent border-none cursor-pointer">
+                <img src="/path/to/your/clickable-element1.png" alt="Click 1" />
+                <span>Click 1</span>
+              </button>
+              </div>
+              <div
+              className="absolute"
+              style={{ top: '350px', left: '400px' }}
+              onClick={() => handleClick('element2')}
+              >
+              <button className="bg-transparent border-none cursor-pointer">
+                <img src="/path/to/your/clickable-element2.png" alt="Click 2" />
+                <span>Click 2</span>
+              </button>
+              </div>
+              <div
+              className="absolute"
+              style={{ top: '90px', left: '750px' }}
+              onClick={() => handleClick('element3')}
+              >
+              <button className="bg-transparent border-none cursor-pointer">
+                <img src="/path/to/your/clickable-element3.png" alt="Click 3" />
+                <span>Click 3</span>
+              </button>
+              </div>
+              <div
+              className="absolute"
+              style={{ top: '530px', left: '60px' }}
+              onClick={() => handleClick('element4')}
+              >
+              <button className="bg-transparent border-none cursor-pointer">
+                <img src="/path/to/your/clickable-element4.png" alt="Click 4" />
+                <span>Click 4</span>
+              </button>
+              </div>
+              <div
+              className="absolute"
+              style={{ top: '550px', left: '400px' }}
+              onClick={() => handleClick('element5')}
+              >
+              <button className="bg-transparent border-none cursor-pointer">
+                <img src="/path/to/your/clickable-element5.png" alt="Click 5" />
+                <span>Click 5</span>
+              </button>
+              </div>
+              <div
+              className="absolute"
+              style={{ top: '500px', left: '750px' }} 
+              onClick={() => handleClick('element6')}
+              >
+              <button className="bg-transparent border-none cursor-pointer">
+                <img src="/path/to/your/clickable-element6.png" alt="click 6" />
+                <span>Click 6</span>
+              </button>
+              </div>
+            </div>
+            ) : (
+              <QuestionsModal onClose={onClose} questions={questions} />
+            )}
+        </div>
       </div>
     </div>
   );
